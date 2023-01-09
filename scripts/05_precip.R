152eba76abf01e0befba7c44f905f933451aecf9
mfarragher7 <62773226+mfarragher7@users.noreply.github.com>
2023-01-09 09:16:23 -0500

    precip work

--- scripts/05_precip.R
+++ scripts/05_precip.R
@@ -9,7 +9,7 @@ library(ggplot2)
 
 
 #load data
-precip = read.csv("C:/Users/CWD2-Matt/OneDrive/Database/dbCWD/db.raw/db.cwd/winthrop.precip_1976-2022.csv")
+precip = read.csv('~/GitHub/dbCWD/db.raw/db.cwd/winthrop.precip_1976-2022.csv')
 str(precip)
 
 p.summary = precip %>% 
@@ -22,19 +22,22 @@ p.summary = precip %>%
             monthly.sd = sd(precip.in)) 
 
 
-p.jan.july = precip %>% 
+p.jan.jun = precip %>% 
   gather(Month, precip.in,
-         Jan, Feb, Mar, Apr, May, Jun, Jul,
+         Jan, Feb, Mar, Apr, May, Jun,
          na.rm = F, convert = F) %>% 
   group_by(Year) %>% 
-  summarize(jan.to.jul.total = sum(precip.in))
+  summarize(jan.to.jun.total = sum(precip.in))
 
 #merge summary dfs
-p.summary = merge(p.summary, p.jan.july, by='Year')
+p.summary = merge(p.summary, p.jan.jun, by='Year')
 
 #percentage of jan-jul total to whole yr
-p.summary$jan.jul.portion = (p.summary$jan.to.jul.total / p.summary$annual.total)
+p.summary$jan.jun.portion = (p.summary$jan.to.jun.total / p.summary$annual.total)
   
+
+
+
 
 #plots ################
             
@@ -69,11 +72,16 @@ ggplot(preciplong, aes(x=date, y=precip.in)) +
 
 #color by month
 ggplot(preciplong, aes(x=date, y=precip.in, color=month)) +
-  geom_line()
+  geom_line() +
+  labs(title='Variability in Monthly Precipitation 1976-2022, measured at CWD',
+       x='Date',
+       y='Precipitation (in)',
+       color='Month') +
+  theme_bw()
 
 
-#jan through july
-jj = c('Jan','Feb','Mar','Apr','May','Jun','Jul')
+#jan through june
+jj = c('Jan','Feb','Mar','Apr','May','Jun')
 prejj = preciplong %>% filter(month %in% jj)
 
 ggplot(prejj, aes(x=date, y=precip.in)) + 
@@ -88,24 +96,73 @@ ggplot(p.summary, aes(x=Year, y=annual.total)) +
   geom_point(shape=19) +
   stat_smooth(method="loess", linewidth=0.75, se=F, show.legend=F)
 
-#jan-july totals each year
-ggplot(p.summary, aes(x=Year, y=jan.to.jul.total)) +
+#jan-june totals each year
+ggplot(p.summary, aes(x=Year, y=jan.to.jun.total)) +
   geom_point(shape=19) +
   stat_smooth(method="loess", linewidth=0.75, se=F, show.legend=F)
 
 
 #combine annual total and jan-july portion together
-precip.annual.jj = p.summary %>% 
-  pivot_longer(cols=c(annual.total, jan.to.jul.total),
+precip.annual.jj = p.summary
+precip.annual.jj$jul.to.dec.total = precip.annual.jj$annual.total - precip.annual.jj$jan.to.jun.total
+
+
+#longer for gggg
+precip.annual.jj = precip.annual.jj %>% 
+  pivot_longer(cols=c(jul.to.dec.total, jan.to.jun.total),
                names_to = 'timespan',
                values_to = 'precip.in')
+
+#rename vars
+precip.annual.jj = precip.annual.jj %>% 
+  mutate(timespan=replace(timespan,grepl('jan',timespan),'Jan-Jun')) %>% 
+  mutate(timespan=replace(timespan,grepl('jul',timespan),'Jul-Dec'))
+
 
 
 ggplot(precip.annual.jj,
        aes(x=Year, y=precip.in, color=timespan)) +
   geom_point(shape=19) +
   geom_line(linetype=3) +
-  stat_smooth(method="loess", linewidth=0.75, se=F, show.legend=F)
+  stat_smooth(method="loess", size=0.75, se=F, show.legend=F) +
+  labs(title='Annual Total Precipitation 1976-2022, measured at CWD',
+       color=NULL,
+       y='Precipitation (in)',
+       x='Year') +
+  theme_bw()
+
+
+
+
+
+
+
+9#as columns instead of points. bleh
+ggplot(p.summary, aes(x=Year, y=annual.total)) +
+  geom_bar(stat='identity', color='white', fill='lightblue', width=1, position =position_dodge(0.1)) +
+  labs(title='Annual Total Precipitation 1976-2022, measured at CWD',
+       y='Precipitation (in)',
+       x='Year') +
+  theme_bw()
+  
+
+
+
+ggplot(precip.annual.jj,
+       aes(x=Year, y=precip.in, color=timespan, fill=timespan)) +
+  geom_bar(position="stack", stat="identity", width=0.5) +
+  labs(title='Annual Total Precipitation 1976-2022, measured at CWD',
+       color=NULL,
+       fill=NULL,
+       y='Precipitation (in)',
+       x='Year') +
+  theme_bw()
+
+
+
+
+
+
 
 
 
@@ -114,13 +171,32 @@ ggplot(precip.annual.jj,
 
 p22 = preciplong %>% filter(Year==2022)
 p22$cumulative.precip = cumsum(p22$precip.in)
+str(p22)
+p22$month
+
+p22$month = factor(p22$month, levels=c('Jan',
+                                       'Feb',
+                                       'Mar',
+                                       'Apr',
+                                       'May',
+                                       'Jun',
+                                       'Jul',
+                                       'Aug',
+                                       'Sep',
+                                       'Oct',
+                                       'Nov',
+                                       'Dec'))
 
 
+ggplot(p22, aes(x=month, y=cumulative.precip)) +
+  geom_col(color='blue', fill='lightblue') +
+  #scale_x_date(date_labels="%b", date_breaks='1 month') +
+  scale_y_continuous(limits=(c(0,50))) +
+  labs(title='2022 Cumulative Precipitation, measured at CWD',
+       y='Precipitation (in)',
+       x='Month')+
+  theme_bw()
 
-ggplot(p22, aes(x=date, y=cumulative.precip)) +
-  geom_col() +
-  scale_x_date(date_labels="%b", date_breaks='1 month')
-  
 
 
 
